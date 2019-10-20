99de4491741987c0fe7f483b70bc8f142a8ba747
sandhyaganapathy <sandhyaganapathy1@gmail.com>
2019-10-20 19:20:03 +0800

    Initial setup

---
+++ Desktop/SampleProject/SampleProject/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SampleProject
+//
+//  Created by venao8 on 20/10/19.
+//  Copyright © 2019 venco4. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
